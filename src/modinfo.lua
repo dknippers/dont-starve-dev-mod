8909b609fe6020278ab03fdf46b9f14cdf21831b
Daniël Knippers <dknippers@gmail.com>
2018-12-25 12:27:53 +0100

    Initial commit

---
+++ src/modinfo.lua
@@ -0,0 +1,18 @@
+name = "Development Mod"
+description = "Mod that loads various development only code"
+author = "dani"
+version = "0.0.1"
+forumthread = ""
+api_version = 6
+api_version_dst = 10
+
+dont_starve_compatible = true
+reign_of_giants_compatible = true
+shipwrecked_compatible = true
+dst_compatible = true
+
+all_clients_require_mod = false
+client_only_mod = true
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
