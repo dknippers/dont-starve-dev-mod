656134edc69e641e0a7e02c38954d92c218bb4de
Daniël Knippers <dknippers@gmail.com>
2019-01-16 20:12:05 +0100

    Added Portal / SpawnEquipment / BeaverMode / GetPlayerPosition

--- src/modmain.lua
+++ src/modmain.lua
@@ -1,6 +1,8 @@
 GLOBAL.CHEATS_ENABLED = true
 GLOBAL.require('debugkeys')
 
+local SpawnPrefab = GLOBAL.SpawnPrefab
+
 local state = {
   -- Set on SimPostInit
   is_mastersim = true,
@@ -11,7 +13,7 @@ local state = {
 local fn = {}
 
 function fn.GetPlayer()
-  if is_dst then
+  if state.is_dst then
     return GLOBAL.ThePlayer
   else
     return GLOBAL.GetPlayer()
@@ -121,7 +123,7 @@ end
 function fn.NextDay()
   if state.is_dst then
     if state.is_mastersim then
-      TheWorld:PushEvent("ms_nextcycle")
+      GLOBAL.TheWorld:PushEvent("ms_nextcycle")
     else
       GLOBAL.c_remote('TheWorld:PushEvent("ms_nextcycle")')
     end
@@ -130,11 +132,59 @@ function fn.NextDay()
   end
 end
 
+function fn.GetPlayerPosition()
+  local player = fn.GetPlayer()
+  return player and player:GetPosition()
+end
+
+function fn.CelestialPortalWithMoonRock()
+  if not state.is_dst then
+    print("DST only")
+  elseif state.is_mastersim then
+    GLOBAL.c_spawn("multiplayer_portal_moonrock")
+    GLOBAL.c_spawn("moonrockidol", 5)
+  else
+    GLOBAL.c_remote('c_spawn("multiplayer_portal_moonrock")')
+    GLOBAL.c_remote('c_spawn("moonrockidol", 5)')
+  end
+end
+
+function fn.SpawnEquipment()
+  local equipment = { "spear", "axe", "goldenaxe", "goldenshovel", "pickaxe", "goldenpickaxe", "hammer", "armorwood" }
+
+  for i=1, 3 do
+    for _, prefab in ipairs(equipment) do
+      if state.is_mastersim then
+        GLOBAL.c_spawn(prefab, 1)
+      else
+        GLOBAL.c_remote('c_spawn("'..prefab..'", 1)')
+      end
+    end
+  end
+end
+
+function fn.BeaverMode()
+  local player = fn.GetPlayer()
+  local beaverness = fn.GetComponent(player, "beaverness")
+  if not beaverness then return end
+
+  local is_beaver = not state.is_dst and beaverness:IsBeaver() or beaverness:GetPercent() > 0
+  if is_beaver then
+    beaverness:SetPercent(0)
+  else
+    beaverness:SetPercent(1)
+  end
+end
+
 GLOBAL._reveal = fn.RevealMap
 GLOBAL._god = fn.GodMode
 GLOBAL._dump = fn.DumpTable
 GLOBAL._inv = fn.GetInventory
-GLOBAL._nextday = fn.NextDay
+GLOBAL._nd = fn.NextDay
+GLOBAL._portal = fn.CelestialPortalWithMoonRock
+GLOBAL._pos = fn.GetPlayerPosition
+GLOBAL._eq = fn.SpawnEquipment
+GLOBAL._beaver = fn.BeaverMode
 
 AddSimPostInit(function()
   state.is_mastersim = fn.IsMasterSim()
